cfb2d9b4b9db1e39dee4e88bd298ad4cd059fae4
Vishwas <vishwasblaze@gmail.com>
2018-11-21 00:16:40 +0530

    Ejs middleware

--- Customer_App_Express/app.js
+++ Customer_App_Express/app.js
@@ -11,15 +11,33 @@ var app = express();
 
 app.use(logger); */
 
+//View Engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 //Set Static Path
-app.use(express.static(path.join(__dirname,'public')));
+/* app.use(express.static(path.join(__dirname,'public'))); */
 
+/* var people = [
+    {
+    name: 'Jeff',
+    age: 30
+    },
+    {
+        name: 'Sara',
+        age: 22
+    }
+]
+ */
 app.get('/',function(req,res){
-    res.send('Hello World');
+
+    res.render('index',{
+        title: 'Customer'
+    });
 });
 
 app.listen(3000,function() {
